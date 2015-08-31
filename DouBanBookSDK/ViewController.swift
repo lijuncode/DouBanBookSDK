37a396c631a060c040dfba3f18a67fdb34f1951b
lijun <likumb@163.com>
2015-08-31 09:27:02 +0800

    Initial Commit

---
+++ DouBanBookSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouBanBookSDK
+//
+//  Created by  李俊 on 15/8/31.
+//  Copyright (c) 2015年  李俊. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DouBanBookSDKTests/DouBanBookSDKTests.swift
@@ -0,0 +1,36 @@
+//
+//  DouBanBookSDKTests.swift
+//  DouBanBookSDKTests
+//
+//  Created by  李俊 on 15/8/31.
+//  Copyright (c) 2015年  李俊. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DouBanBookSDKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
